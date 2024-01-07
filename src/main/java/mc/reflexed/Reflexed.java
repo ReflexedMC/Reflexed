a9d0e3feb419d18f4a031cbf97539ca69c2280bf
NickRest <nickrestemail@gmail.com>
2024-01-07 16:28:30 -0500

    Base testing

--- src/main/java/mc/reflexed/Reflexed.java
+++ src/main/java/mc/reflexed/Reflexed.java
@@ -1,12 +1,41 @@
 package mc.reflexed;
 
+import lombok.Getter;
+import mc.reflexed.command.CommandManager;
+import mc.reflexed.commands.TestCommand;
+import mc.reflexed.event.EventManager;
+import mc.reflexed.event.data.EventInfo;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public final class Reflexed extends JavaPlugin {
+
+    private final CommandManager commandManager;
+    private final EventManager eventManager;
+
+    public Reflexed() {
+        this.commandManager = new CommandManager(this);
+        this.eventManager = new EventManager(this);
+    }
 
     @Override
     public void onEnable() {
+        eventManager.onEnable();
 
+        commandManager.register(new TestCommand());
+        eventManager.register(this);
+    }
+
+    @EventInfo
+    public void onMove(PlayerMoveEvent e) {
+        e.getPlayer().sendMessage("Stop moving!");
+    }
+
+    public static Reflexed get() {
+        return getPlugin(Reflexed.class);
     }
 
 }
---
+++ src/main/java/mc/reflexed/commands/TestCommand.java
@@ -0,0 +1,26 @@
+package mc.reflexed.commands;
+
+import mc.reflexed.Reflexed;
+import mc.reflexed.command.CommandManager;
+import mc.reflexed.command.FlexedCommand;
+import mc.reflexed.command.ICommandExecutor;
+import mc.reflexed.command.data.CommandInfo;
+import org.bukkit.command.CommandSender;
+
+@CommandInfo(name = "unregister")
+public class TestCommand implements ICommandExecutor {
+
+    @Override
+    public boolean execute(CommandSender sender, String[] args, String label) {
+        sender.sendMessage("Unregistering command...");
+
+        FlexedCommand command = Reflexed.get().getCommandManager().getCommandFromExecutor(this);
+        Reflexed.get().getCommandManager().unregister(command);
+        return false;
+    }
+
+    @Override
+    public String[] tabComplete(CommandSender sender, String[] args, String label) {
+        return new String[] {args.length > 0 ? "test" + args.length : "test"};
+    }
+}
