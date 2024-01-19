aacf0b64921093910e4b00bfff68e5d1217a3ff1
godmodeD <djlevy26@gmail.com>
2024-01-18 20:08:05 -0500

    fixed combo command

--- src/main/java/mc/reflexed/command/commands/ComboCommand.java
+++ src/main/java/mc/reflexed/command/commands/ComboCommand.java
@@ -14,7 +14,7 @@ import org.bukkit.command.CommandSender;
 public class ComboCommand implements ICommandExecutor {
     @Override
     public boolean execute(CommandSender commandSender, String[] strings, String s) {
-        Reflexed.get().setComboMode(!Reflexed.get().isComboMode());
+        Reflexed.get().setComboMode(true);
         ChatUtil.broadcast("§aCombo mode has been " + (Reflexed.get().isComboMode() ? "enabled" : "disabled") + "!");
         return false;
     }
