73fd63329feb705e003061918a7a8de6c07e2f88
godmodeD <djlevy26@gmail.com>
2024-01-18 22:23:07 -0500

    better message for combo mode

--- src/main/java/mc/reflexed/command/commands/ComboCommand.java
+++ src/main/java/mc/reflexed/command/commands/ComboCommand.java
@@ -21,7 +21,7 @@ public class ComboCommand implements ICommandExecutor {
                 player.setNoDamageTicks(10);
             });
         }
-        ChatUtil.broadcast("§aCombo mode has been " + (Reflexed.get().isComboMode() ? "enabled" : "disabled") + "!");
+        ChatUtil.broadcast("§aCombo mode has been §d" + (Reflexed.get().isComboMode() ? "enabled" : "disabled") + "§a!");
         return false;
     }
 }
