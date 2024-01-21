360c84f633c33560638b9ce261b2f94c118adb9e
NickRest <nickrestemail@gmail.com>
2024-01-21 12:37:35 -0500

    debug

--- src/main/java/mc/reflexed/ac/GroundSpoofA.java
+++ src/main/java/mc/reflexed/ac/GroundSpoofA.java
@@ -5,6 +5,7 @@ import mc.reflexed.ac.check.data.CheckInfo;
 import mc.reflexed.ac.check.data.CheckType;
 import mc.reflexed.event.data.EventInfo;
 import mc.reflexed.util.ACUtil;
+import mc.reflexed.util.ChatUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -20,11 +21,27 @@ public class GroundSpoofA extends Check {
     @EventInfo
     @SuppressWarnings("deprecation")
     public void onMove(Player player, PlayerMoveEvent e) {
+        if(areBlocksAround(player)) {
+            ChatUtil.message("Blocks around", player);
+            return;
+        }
+
         if(realOnGroundTick == 0 && onGroundTick > 5) {
             flag("onGroundTick=" + onGroundTick, "realOnGroundTick=" + realOnGroundTick);
         }
 
         this.realOnGroundTick = (ACUtil.isOnGround(player)) ? this.realOnGroundTick + 1 : 0;
         this.onGroundTick = (player.isOnGround()) ? this.onGroundTick + 1 : 0;
+    }
+
+    public boolean areBlocksAround(Player player) {
+        for(int x = -1; x < 2; x++) {
+            for(int z = -1; z < 2; z++) {
+                if(player.getWorld().getBlockAt(player.getLocation().add(x, -1, z)).getType().isSolid()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
