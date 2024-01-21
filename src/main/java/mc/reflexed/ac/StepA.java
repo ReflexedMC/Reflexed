406384673231a6a56cc8b88627ad749d36d7dc82
NickRest <nickrestemail@gmail.com>
2024-01-21 13:41:25 -0500

    fix false dylan called out

--- src/main/java/mc/reflexed/ac/StepA.java
+++ src/main/java/mc/reflexed/ac/StepA.java
@@ -7,6 +7,7 @@ import mc.reflexed.event.data.EventInfo;
 import mc.reflexed.util.ACUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 @CheckInfo(
         name = "Step",
@@ -15,13 +16,23 @@ import org.bukkit.event.player.PlayerMoveEvent;
 )
 public class StepA extends Check {
 
+    private double lastMotionY;
+
     @EventInfo
     public void onMove(Player player, PlayerMoveEvent e) {
         double motionY = e.getTo().getY() - e.getFrom().getY();
 
-        if(motionY > 0.6 && ACUtil.areBlocksAround(player)) {
+        double lastMotionY = this.lastMotionY;
+        this.lastMotionY = motionY;
+
+        if(lastMotionY > 0.6 && motionY > 0.6 && ACUtil.areBlocksAround(player)) {
             flag("motionY=" + motionY);
         }
+    }
+
+    @EventInfo
+    public void onTeleport(Player player, PlayerTeleportEvent e) {
+        this.lastMotionY = 0;
     }
 
 }
